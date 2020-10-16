fcceb0a8bc8d0e56f9049ff267d2b32951cc79c0
FrancescoS <francesco.salerno2294@gmail.com>
2020-10-16 16:40:34 +0200

    Work2

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,14 +1,20 @@
 @extends('layouts.app')
 @section('content')
-    <form action="{{route('clients.store')}}" method="post">
+    {{-- <form action="{{route('clients.store')}}" method="post"> --}}
+    <form action="{{(!empty($client)) ? route('clients.update', $client->id) : route('clients.store')}}" method="post">
     @csrf
-    @method('POST')
+    @if(!empty($client))
+        @method('PATCH')
+        <input type="hidden" name="id" value="{{ $client->id }}">
+    @else
+        @method('POST')
+    @endif
     <label for="title">Nome</label>
-    <input type="text" name="name" placeholder="Nome" id="name" value="{{ old('name') }}">
+    <input type="text" name="name" placeholder="Nome" id="name" value="{{ (!empty($client)) ? $client->nome : old('name') }}">
     <label for="content">Cognome</label>
-    <input type="text" name="lastname" placeholder="Cognome" id="lastname" value="{{ old('lastname') }}">
+    <input type="text" name="lastname" placeholder="Cognome" id="lastname" value="{{ (!empty($client)) ? $client->cognome : old('lastname') }}">
     <label for="content">Età</label>
-    <input type="text" name="eta" placeholder="Età" id="eta" value="{{ old('eta') }}">
+    <input type="text" name="eta" placeholder="Età" id="eta" value="{{ (!empty($client)) ? $client->eta : old('eta') }}">
     <input type="submit" value="Invia">
     </form>
 @endsection
